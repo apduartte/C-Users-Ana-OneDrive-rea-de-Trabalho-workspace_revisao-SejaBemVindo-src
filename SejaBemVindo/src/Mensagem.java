356ba26c50a79e69f872b3ee472c63efb57379f2
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2022-03-15 09:53:51 -0300

    Inclusão de nome e gênero

--- SejaBemVindo/src/Mensagem.java
+++ SejaBemVindo/src/Mensagem.java
@@ -11,8 +11,20 @@ public class Mensagem {
 		System.out.println("Sejam bem vindos a nossa aula de revisão Java");
 		System.out.println("Sejam bem vindos a nossa aula de revisão Java");	
 	*/
-		String nome="Tarciano";
+	
+		/*
+		String nome = "Tarciano";
 		System.out.println(nome);
+		int idade = 30;
+		System.out.println(idade);
+		String genero = "M";
+		System.out.println(genero);
+		*/
+		
+		String nome = "Ana";
+		String sobrenome = "Duarte";
+		System.out.println(nome+" "+sobrenome);
+		
 
 	}
 
